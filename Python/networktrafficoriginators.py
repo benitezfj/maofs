137bd71dfaac6a4ca5c06927081234d4ac41afab
Francisco Benitez <benitez.fj@hotmail.com>
2023-08-28 22:45:15 -0400

    Neworktrafficoriginators file was corrected.

--- Python/networktrafficoriginators.py
+++ Python/networktrafficoriginators.py
@@ -93,16 +93,3 @@ def optimize_model(model_name, dataset_name, algorithm_name, iterator):
                           eliminate_duplicates=False)
 
     optimize_and_save(ref_dirs, dataset_name, algorithm, algorithm_name, model, model_name, problem, num_iterations)
-
-
-model_names = ['gnb', 'knn']
-dataset_aliases = ["linux_memory", "linux_disk", "linux_process", "network", "win7", "win10"]
-algorithm_aliases = ["NSGA-III", "NSGA-II", "RVEA", "MOEA/D"]
-num_iterations = 12
-
-for model_name in model_names:
-    for dataset_alias in dataset_aliases:
-      for algorithm_alias in algorithm_aliases:
-        algorithm_name = algorithm_alias
-        dataset_name = dataset_alias
-        optimize_model(model_name, dataset_name, algorithm_name, num_iterations)
